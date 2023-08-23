6d9796955fe0908edaa5a2c26b931c5b2b935709
damac53 <mitchellefetty@gmail.com>
2023-08-23 12:45:56 +0200

    API added

---
+++ app/src/main/java/com/breens/youtubeclonestarter/api/YoutubeApi.kt
@@ -0,0 +1,23 @@
+package com.breens.youtubeclonestarter.api
+
+import com.breens.youtubeclonestarter.utils.Constant.Companion.LIST_OF_VIDEOS
+import com.breens.youtubeclonestarter.utils.Constant.Companion.SNIPPET
+import com.breens.youtubeclonestarter.utils.Constant.Companion.DETAILS
+import com.breens.youtubeclonestarter.utils.Constant.Companion.KEY
+import com.breens.youtubeclonestarter.utils.Constant.Companion.MOST_POPULAR
+import com.breens.youtubeclonestarter.utils.Constant.Companion.REGION_CODE
+import com.breens.youtubeclonestarter.utils.Constant.Companion.STATISTICS
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface YoutubeApi {
+
+    @GET(LIST_OF_VIDEOS)
+    suspend fun fetchVideos(
+        @Query("part")part :String ="$SNIPPET,$DETAILS,$STATISTICS",
+        @Query("chart")chart : String = MOST_POPULAR,
+        @Query("regionCode") regionCode : String = REGION_CODE,
+        @Query("key")key:String = KEY
+
+    )
+}
